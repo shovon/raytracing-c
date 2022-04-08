9f3e243a4f06475842e51656ca2e0b97059f9576
Sal Rahman <salehen.rahman@gmail.com>
2022-04-07 23:53:26 -0700

    Camera working

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include "hitable.h"
 #include "hitable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 float hit_sphere(vec3 center, float radius, ray r)
 {
@@ -44,6 +45,7 @@ int main()
 {
   int nx = 200;
   int ny = 100;
+  int ns = 100;
   printf("P3\n%d %d\n255\n", nx, ny);
 
   vec3 lower_left_corner = {-2.0, -1.0, -1.0};
@@ -51,27 +53,28 @@ int main()
   vec3 vertical = {0.0, 2.0, 0.0};
   vec3 origin = {0.0, 0.0, 0.0};
 
-  hitable list = make_hitable_list(2);
-  hitable_list hl = *((hitable_list *)list.item);
+  hitable world = make_hitable_list(2);
+  hitable_list hl = *((hitable_list *)world.item);
   hl.list[0] = make_sphere(
       make_vec3(0, 0, -1), 0.5);
   hl.list[1] = make_sphere(
       make_vec3(0, -100.5, -1), 100);
 
+  camera cam = make_camera();
+
   for (int j = ny - 1; j >= 0; j--)
   {
     for (int i = 0; i < nx; i++)
     {
-      float u = ((float)i) / ((float)nx);
-      float v = ((float)j) / ((float)ny);
-      ray r = {
-          origin,
-          vec3_add(
-              lower_left_corner,
-              vec3_add(
-                  vec3_scalar_mul(horizontal, u),
-                  vec3_scalar_mul(vertical, v)))};
-      vec3 col = color(r, list);
+      vec3 col = {0, 0, 0};
+      for (int s = 0; s < ns; s++)
+      {
+        float u = (((float)i) + drand48()) / ((float)nx);
+        float v = (((float)j) + drand48()) / ((float)ny);
+        ray r = camera_get_ray(cam, u, v);
+        col = vec3_add(col, color(r, world));
+      }
+      col = vec3_scalar_div(col, ns);
       int ir = (int)(255.99 * col.e0);
       int ig = (int)(255.99 * col.e1);
       int ib = (int)(255.99 * col.e2);
