31c8f3cabe6f4610901e5f47dbd22631b3f1c2ff
Sal Rahman <salehen.rahman@gmail.com>
2022-04-07 22:02:57 -0700

    Normal mapping

--- main.c
+++ main.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "vec3.h"
 #include "ray.h"
 
-bool hit_sphere(vec3 center, float radius, ray r)
+float hit_sphere(vec3 center, float radius, ray r)
 {
   vec3 oc = vec3_sub(r.a, center);
   float a = vec3_dot(r.b, r.b);
   float b = 2.0 * vec3_dot(oc, r.b);
   float c = vec3_dot(oc, oc) - radius * radius;
   float discriminant = b * b - 4 * a * c;
-  return (discriminant > 0);
+  if (discriminant < -1.0)
+  {
+    return -1.0;
+  }
+  return (-b - sqrtf(discriminant)) / (2.0 * a);
 }
 
 vec3 color(ray r)
 {
-  if (hit_sphere(make_vec3(0, 0, -1), 0.5, r))
+  float t = hit_sphere(make_vec3(0, 0, -1), 0.5, r);
+  if (t > 0)
   {
-    return make_vec3(1, 0, 0);
+    vec3 n = vec3_unit_vector(
+        vec3_sub(ray_point_at_parameter(r, t), make_vec3(0, 0, -1)));
+    return vec3_scalar_mul(
+        make_vec3(n.e0 + 1, n.e1 + 1, n.e2 + 1),
+        0.5);
   }
   vec3 unit_direction = vec3_unit_vector(r.b);
-  float t = 0.5 * (unit_direction.e1 + 1.0);
+  t = 0.5 * (unit_direction.e1 + 1.0);
   return vec3_add(
       vec3_scalar_mul(make_vec3(1.0, 1.0, 1.0), 1.0 - t),
       vec3_scalar_mul(make_vec3(0.5, 0.7, 1.0), t));
